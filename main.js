94c12b66c6943123b62350e1d20c78f37a366deb
Julie Pan <ycp217@nyu.edu>
2015-05-25 02:07:49 -0700

    can get building shadow length of all targeted nodes

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 var SunCalc = require('suncalc');
 var request = require('request');
 
-var sunPos = SunCalc.getPosition(new Date(), 37.0000, -120.0000);
-console.log(sunPos);
-
-var buildingHeight = 10; // in meters
-var shadowLen = buildingHeight / Math.tan(sunPos.altitude);
-
-console.log('shadow length', shadowLen, 'meters\n');
-
-
-var query = "[out:json][timeout:25];(node[%22height%22][%22building%22](40.769,-74.0,40.77,-73.99);way[%22height%22][%22building%22](40.769,-74.0,40.77,-73.99);relation[%22height%22][%22building%22](40.769,-74.0,40.77,-73.99););out;"
-
-
-
 
 /*
- * QL formatting node id for Overpass API
+ * get right inputs for formattingRect(), use lat/lon of A and B to contruct a box
+ * */
+var parseInput = function(A, B) {
+  var arr_A = A.split(',');
+  var arr_B = B.split(',');
+  return arr_A.concat(arr_B);
+}
+
+/*
+ * set coordinates area to get building data from Overpass
+ * Adjust top/right increment values if not getting values in getPoint().
+ * This usually means that the area selected is too large to process.
+ * */
+var formattingRect = function(bottom, left, top, right) {
+  var tmp;
+  if (bottom > top) { // make sure box is sound
+    tmp = bottom;
+    bottom = top;
+    top = tmp;
+  }
+  if (left < right) {
+    tmp = left;
+    left = right;
+    right = tmp;
+  }
+
+  var cords = "(" + bottom + "," + left + "," + top + "," + right + ");";
+  return  "[out:json][timeout:25];(" + 
+    "node[%22height%22][%22building%22]" + cords + 
+    "way[%22height%22][%22building%22]" + cords +
+    "relation[%22height%22][%22building%22]" + cords +
+    ");out;";
+}
+
+/*
+ * QL formatting node id
  * @param id - unique node id for OpenStreetMap
  * */
 var getNodeName = function(id) {
@@ -24,8 +46,8 @@ var getNodeName = function(id) {
 }
 
 /*
- * QL formatting for making API calls to Overpass API node, way, relations, and etc.
- * @param blcok - content of search
+ * QL formatting for getting node, way, relations, and etc data before API call.
+ * @param block - content of search
  * */
 var getQuery = function(block){
   return "[out:json][timeout:25];(" + block + ");out;";
@@ -33,19 +55,25 @@ var getQuery = function(block){
 
 
 /*
- * @param query - finalize url formatting to make API call
+ * @param query - finalize url formatting to make API call, used inside getHeight()
  * */
 var apiCall = function(query) {
   var url = "http://overpass-api.de/api/interpreter?data="
   return url + query;
 }
 
-var block = getNodeName(644184848) + getNodeName(3316656367);
 
 // initialize variables to store callbacks
 var nodeList = new Object();
 
-var getHeight = function(query) {
+/*
+ * MAIN DRIVER:
+ * Collect height of all nodes.ways in a designated block
+ * */
+var getHeight = function(A, B) {
+  var str = parseInput(A, B);
+  var query = formattingRect(str[0], str[1], str[2], str[3]);
+
   request(apiCall(query), function(err, res, body) {
     if (!err && res.statusCode == 200) {
       var nodeBlock = "";
@@ -58,8 +86,6 @@ var getHeight = function(query) {
           nodeList[nodeId] = body['elements'][i]['tags']['height'];
         }
       }
-      //console.log(JSON.stringify(nodeList));
-      //console.log(nodeBlock);
       getPoints(nodeBlock);
     } else {
       console.log('getHeight() failed');
@@ -67,25 +93,90 @@ var getHeight = function(query) {
   });
 }
 
-
+var cordList = new Object();
 /*
-* Overpass API request can't handle more than a certain number of nodes, so keep it small
-* */
+ * Overpass API request can't handle more than a certain number of nodes, so keep it small!
+ * Outputs nodeIds and their corresponding latitude/longitude
+ * @param nodeBlock - correctly formated QL code to get nodes from Overpass API
+ * */
 var getPoints = function(nodeBlock) {
   request(apiCall(getQuery(nodeBlock)), function(err, res, body) {
     if (!err && res.statusCode == 200) {
       body = JSON.parse(body);
       for (var i = 0; i < body['elements'].length; i++) {
-        console.log('index', i);
-        console.log(body['elements'][i]['id'], body['elements'][i]['lat']);
+        nodeId = body['elements'][i]['id'];
+        cordList[nodeId] = ('' + body['elements'][i]['lat'] + ',' + body['elements'][i]['lon'] + '');
       }
+      getShadow();
     } else {
-      console.log('getPoints() failed');
+      console.log('getPoints() failed: rectangle sized too big');
     }
   });
 }
 
-getHeight(query);
+
+/*
+ * simple Pythagorean theorem to get shadow length
+ */
+var getShadowLength = function(buildingHeight, sunAltitude) {
+  return buildingHeight / Math.tan(sunAltitude);
+}
+
+var shadowList = new Object();
+/*
+ * Calculates the shadow lenght of each building depending on sun's altitude at that time
+ * */
+var getShadow = function() {
+  var firstKey = Object.keys(cordList)[0];
+  var str = cordList[firstKey].split(',');
+  //var tempDate = 'Mon May 25 2015 09:50:04 GMT-0700 (PDT)' // REPLACE TEMP WITH new Date() WHEN SUN IS UP!!!!!
+  var position = SunCalc.getPosition(new Date(), str[0], str[1]);
+  /*
+  var times = SunCalc.getTimes(new Date, str[0], str[1]);
+  if (new Date() > times.sunset || new Date() < times.sunrise) {
+    console.log("This app only works when the sun is up, yo!")
+  } else {
+    for (key in cordList) {
+      console.log(nodeList[key]);
+      shadowList[key] = getShadowLength(nodeList[key], position.altitude);
+    }
+    console.log(shadowList);
+  }
+  UNCOMMENT THIS AFTER DEMO AND PEOPLE CAN'T USE IT AT NIGHT ANYMORE
+  */
+
+  // REMEMBER TO TEST WHEN THE SUN IS UP
+  for (key in cordList) {
+    shadowList[key] = getShadowLength(nodeList[key], position.altitude);
+  }
+  console.log(shadowList);
+}
+
+
+
+
+// sample points to build path around
+// (40.731847, -73.997396) - Washington Square Park coordinates
+// (40.749045, -74.004902) - the High Line
+var A = "40.739847, -73.999396";
+var B = "40.740045, -74.000902";
+
+getHeight(A, B);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
